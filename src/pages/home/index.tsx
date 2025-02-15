3b41536669eaf5299383db16167e695311c5f3d1
G1Coding <32190250@dankook.ac.kr>
2025-02-15 22:02:03 +0900

    feat: home component ui 완성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,12 +10,39 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post list</div>
+      <div className="post__navigation">
+        <div className="post__navigation-active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/post/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">{"지원"}</div>
+                <div className="post__date">{"2025-02-15"}</div>
+              </div>
+              <div className="post__title">
+                {" "}
+                {"게시글"} {index}
+              </div>
+              <div className="post__text">
+                {"안녕하세요 블로그 등업 신청합니다!!"}
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">{"삭제"}</div>
+                <div className="post__edit">{"수정"}</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
 
       <footer>
-        <div>menu 1</div>
-        <div>menu 2</div>
-        <div>menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
