f162557e35cb307deb390edd0cde14fc2bcab4b2
G1Coding <32190250@dankook.ac.kr>
2025-02-16 00:45:34 +0900

    feat: 게시글 페이지 구현

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,13 @@
-export default function PostList() {
-    return(
-        <h1>Post List Page</h1>
-    )
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import PostList from "../../components/PostList";
+
+export default function PostListPage() {
+  return (
+    <>
+      <Header />
+      <PostList hasNavigation={false} />
+      <Footer />
+    </>
+  );
 }
