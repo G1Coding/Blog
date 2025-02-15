e7450d595ea2186597873a07d93c896a8130efd6
G1Coding <32190250@dankook.ac.kr>
2025-02-16 00:26:37 +0900

    feat: 게시글 상세페이지 구현

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,24 @@
+export default function PostDetail() {
+  return (
+    <div className="post__detail">
+      <div className="post__box">
+        <div className="post__title">
+          post 내용 블로그 내용용post 내용 블로그 내용용post 내용 블로그
+          내용용post 내용 블로그 내용용post 내용 블로그 내용용
+        </div>
+        <div className="post__profile-box">
+          <div className="post__profile"></div>
+          <div className="post__author-name">{"지원"}</div>
+          <div className="post__date">{"2025-02-15"}</div>
+        </div>
+        <div className="post__utils-box">
+          <div className="post__delete">{"삭제"}</div>
+          <div className="post__edit">{"수정"}</div>
+        </div>
+        <div className="post__text">
+          {"안녕하세요 블로그 등업 신청합니다!!"}
+        </div>
+      </div>
+    </div>
+  );
+}
